4ebd72cdb0e25c5c96e5e6cf619055073e959a61
Chinnary <sahaja.belaganti@nexturn.com>
2024-10-16 17:33:10 +0530

    consumed disount in basket microservice

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,9 @@
 using Asp.Versioning;
+using Basket.Application.GrpcService;
 using Basket.Application.Handlers;
 using Basket.Core.Repositories;
 using Basket.Infrastructure.Repositories;
+using Discount.Grpc.Protos;
 using System.Reflection;
 
 
@@ -38,6 +40,8 @@ builder.Services.AddStackExchangeRedisCache(options =>
 });
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<DiscountGrpcService>();
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(cfg => cfg.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 
 var app = builder.Build();
 
--- Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
@@ -1,8 +1,10 @@
 ﻿using Basket.Application.Commands;
+using Basket.Application.GrpcService;
 using Basket.Application.Mappers;
 using Basket.Application.Responses;
 using Basket.Core.Entities;
 using Basket.Core.Repositories;
+using Discount.Grpc.Protos;
 using MediatR;
 
 namespace Basket.Application.Handlers
@@ -10,13 +12,20 @@ namespace Basket.Application.Handlers
     public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository)
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
         {
             _basketRepository = basketRepository;
+            _discountGrpcService = discountGrpcService;
         }
         public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
         {
+            foreach (var item in request.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
             var shoppingCart = await _basketRepository.UpsetBasket(new ShoppingCart 
             { 
                 UserName = request.UserName, 
